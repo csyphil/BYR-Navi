9d6636465bc08ab56733248eb590bc67996996e4
philsilver31925 <philsilver31925@gmail.com>
2022-04-01 22:02:49 +0800

    Update hitokoto.js

--- assets/js/hitokoto.js
+++ assets/js/hitokoto.js
@@ -7,9 +7,9 @@ $.getJSON('https://v1.hitokoto.cn', function (data) {
     }).append(data.hitokoto).append(
         $('<div>').addClass('detail').text(data.from)
     );
-// }).fail(function () {
-//     $('#hitokoto').attr({
-//         'href': '//google.philsilver.xyz',
-//         'target': '_blank'
-//     }).html('<i class="lightbulb icon"></i>一言获取失败...<div class="detail">错误</div>');
+}).fail(function () {
+    $('#hitokoto').attr({
+        'href': 'https://google.com/',
+        'target': '_blank'
+    }).html('<i class="lightbulb icon"></i>一言获取失败...<div class="detail">错误</div>');
 });
