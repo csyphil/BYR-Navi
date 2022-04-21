e5bc68f10a49000ab6e0c9a24fc5d27a160bf340
philsilver31925 <philsilver31925@gmail.com>
2022-04-21 22:04:36 +0800

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -23,57 +23,57 @@ $(document).ready(function () {
 $('.ui.inverted.masthead.segment').addClass(`bg${Math.ceil(Math.random() * 14)}`).removeClass('zoomed');
 
 {%- if site.data.analytics.matomo.site_id -%}
-// // analytics
-// $.getJSON('{{ site.data.analytics.matomo.url }}', {
-//     'module': 'API',
-//     'method': 'VisitsSummary.getVisits',
-//     'idSite': '{{ site.data.analytics.matomo.site_id }}',
-//     'period': 'day',
-//     'date': 'yesterday',
-//     'format': 'JSON',
-//     'token_auth': '{{ site.data.analytics.matomo.token }}'
-// }, function (data) {
-//     $('#yesterday-visits').text(data.value);
-// });
-// $.getJSON('{{ site.data.analytics.matomo.url }}', {
-//     'module': 'API',
-//     'method': 'VisitsSummary.getActions',
-//     'idSite': '{{ site.data.analytics.matomo.site_id }}',
-//     'period': 'day',
-//     'date': 'yesterday',
-//     'format': 'JSON',
-//     'token_auth': '{{ site.data.analytics.matomo.token }}'
-// }, function (data) {
-//     $('#yesterday-actions').text(data.value);
-// });
-// function updateAnalytics() {
-//     $.getJSON('{{ site.data.analytics.matomo.url }}', {
-//         'module': 'API',
-//         'method': 'VisitsSummary.getVisits',
-//         'idSite': '{{ site.data.analytics.matomo.site_id }}',
-//         'period': 'day',
-//         'date': 'today',
-//         'format': 'JSON',
-//         'token_auth': '{{ site.data.analytics.matomo.token }}'
-//     }, function (data) {
-//         $('#today-visits').text(data.value);
-//     });
-//     $.getJSON('{{ site.data.analytics.matomo.url }}', {
-//         'module': 'API',
-//         'method': 'VisitsSummary.getActions',
-//         'idSite': '{{ site.data.analytics.matomo.site_id }}',
-//         'period': 'day',
-//         'date': 'today',
-//         'format': 'JSON',
-//         'token_auth': '{{ site.data.analytics.matomo.token }}'
-//     }, function (data) {
-//         $('#today-actions').text(data.value);
-//     });
-// };
-// updateAnalytics();
-// setInterval(function () {
-//     if (!document.hidden) {
-//         updateAnalytics();
-//     };
-// }, 60000);
-// {%- endif -%}
+// analytics
+$.getJSON('{{ site.data.analytics.matomo.url }}', {
+    'module': 'API',
+    'method': 'VisitsSummary.getVisits',
+    'idSite': '{{ site.data.analytics.matomo.site_id }}',
+    'period': 'day',
+    'date': 'yesterday',
+    'format': 'JSON',
+    'token_auth': '{{ site.data.analytics.matomo.token }}'
+}, function (data) {
+    $('#yesterday-visits').text(data.value);
+});
+$.getJSON('{{ site.data.analytics.matomo.url }}', {
+    'module': 'API',
+    'method': 'VisitsSummary.getActions',
+    'idSite': '{{ site.data.analytics.matomo.site_id }}',
+    'period': 'day',
+    'date': 'yesterday',
+    'format': 'JSON',
+    'token_auth': '{{ site.data.analytics.matomo.token }}'
+}, function (data) {
+    $('#yesterday-actions').text(data.value);
+});
+function updateAnalytics() {
+    $.getJSON('{{ site.data.analytics.matomo.url }}', {
+        'module': 'API',
+        'method': 'VisitsSummary.getVisits',
+        'idSite': '{{ site.data.analytics.matomo.site_id }}',
+        'period': 'day',
+        'date': 'today',
+        'format': 'JSON',
+        'token_auth': '{{ site.data.analytics.matomo.token }}'
+    }, function (data) {
+        $('#today-visits').text(data.value);
+    });
+    $.getJSON('{{ site.data.analytics.matomo.url }}', {
+        'module': 'API',
+        'method': 'VisitsSummary.getActions',
+        'idSite': '{{ site.data.analytics.matomo.site_id }}',
+        'period': 'day',
+        'date': 'today',
+        'format': 'JSON',
+        'token_auth': '{{ site.data.analytics.matomo.token }}'
+    }, function (data) {
+        $('#today-actions').text(data.value);
+    });
+};
+updateAnalytics();
+setInterval(function () {
+    if (!document.hidden) {
+        updateAnalytics();
+    };
+}, 60000);
+{%- endif -%}
